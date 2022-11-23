f1865d9f9a2db9786d5a7a481cddc89c98eb8a80
ShemaiahMontilla <shemaiah.montilla@g.msuiit.edu.ph>
2022-11-23 12:54:36 +0800

    Added a file for the configuration and for running the app module

---
+++ config.py
@@ -0,0 +1,11 @@
+from os import getenv
+
+SECRET_KEY = getenv("SECRET_KEY")
+DB_NAME = getenv("DB_NAME")
+DB_USERNAME = getenv("DB_USERNAME")
+DB_PASSWORD = getenv("DB_PASSWORD")
+DB_HOST = getenv("DB_HOST")
+BOOTSTRAP_SERVE_LOCAL = getenv("BOOTSTRAP_SERVE_LOCAL")
+CLOUD_NAME =  getenv("CLOUD_NAME")
+API_KEY = getenv("API_KEY")
+API_SECRET = getenv("API_SECRET")
